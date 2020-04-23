d801eb0cdde57b76d3e8dbdf62857feb13c4a768
jw792 <60716845+jw792@users.noreply.github.com>
2020-04-23 10:16:54 -0400

    update tutorial

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonPage, IonButton, IonContent, IonToolbar, IonTitle } from '@ionic/react';
+import { IonPage, IonButton, IonContent, IonToolbar, IonTitle, IonRow, IonCol } from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
 
 interface Props extends RouteComponentProps {
@@ -11,6 +11,26 @@ const Quiz: React.FC<Props> = ({ quiz }) => {
     <IonPage>
       <IonContent fullscreen class="ion-padding">
         <IonToolbar>
+          <IonCol size="12" size-sm>
+            <div>
+              <b>Thanks for coming! We are glad you came!</b>
+            </div>
+          </IonCol>
+          <IonCol size="12" size-sm>
+            <div>
+              <b>Tutorial Information</b>
+            </div>
+          </IonCol>
+          <IonCol size="12" size-sm>
+            <div>
+              <b>This is the body</b>
+            </div>
+          </IonCol>
+          <IonCol size="12" size-sm>
+            <div>
+              <b>Complete 4 out 5 questions to win FLIR camera</b>
+            </div>
+          </IonCol>
           <IonTitle class="title">{quiz && quiz.name}</IonTitle>
           <IonButton routerLink="/quiz/question">Start Quiz</IonButton>
         </IonToolbar>
