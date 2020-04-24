050dd5cdda772cfe918a617428d0790f58aa3f99
jw792 <60716845+jw792@users.noreply.github.com>
2020-04-23 20:23:41 -0400

    Update Quiz.tsx

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,34 +1,48 @@
-import React from 'react';
 import { IonPage, IonButton, IonContent, IonToolbar, IonTitle, IonRow, IonCol } from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
+import api from "../api";
+import React, { useEffect, useState } from "react";
 
 interface Props extends RouteComponentProps {
   quiz: any;
 }
 
 const Quiz: React.FC<Props> = ({ quiz }) => {
+  const sendGetTutorialRequest = () => {
+    return api.get(
+      `/quiz/${quiz.id}/tutorial`
+    );
+  };
+
+  const [header, setHeader] = useState();
+  const [body, setBody] = useState();
+  const [totalQuestions, setTotalQuestions] = useState();
+
+  useEffect(() => {
+    sendGetTutorialRequest().then(res => {
+      setHeader(res.data.header);
+      setBody(res.data.body);
+      setTotalQuestions(res.data.totalQuestions);
+    });
+  }, [quiz]);
+
   return (
     <IonPage>
       <IonContent fullscreen class="ion-padding">
         <IonToolbar>
           <IonCol size="12" size-sm>
             <div>
-              <b>Thanks for coming! We are glad you came!</b>
+              <b>{header}</b>
             </div>
           </IonCol>
           <IonCol size="12" size-sm>
             <div>
-              <b>Tutorial Information</b>
+              <b>{body}</b>
             </div>
           </IonCol>
           <IonCol size="12" size-sm>
             <div>
-              <b>This is the body</b>
-            </div>
-          </IonCol>
-          <IonCol size="12" size-sm>
-            <div>
-              <b>Complete 4 out 5 questions to win FLIR camera</b>
+              <b>Complete {(String)((Number)(totalQuestions) * 0.6)} out {totalQuestions} questions to win FLIR camera</b>
             </div>
           </IonCol>
           <IonTitle class="title">{quiz && quiz.name}</IonTitle>
