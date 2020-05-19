0eed7774e4fe0030e7b4bb93addead2a1da7a9b7
Evan Welsh <2943271+rockon999@users.noreply.github.com>
2020-05-19 13:40:26 -0500

    Fix production build errors in development.

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -19,11 +19,11 @@ const Quiz: React.FC<Props> = ({ quiz, history }) => {
     return api.get(`/quiz/${quiz.id}/raffle`);
   }, [quiz]);
 
-  const [header, setHeader] = useState();
-  const [body, setBody] = useState();
-  const [totalQuestions, setTotalQuestions] = useState();
-  const [questionRequirement, setQuestionRequirement] = useState();
-  const [prize, setPrize] = useState();
+  const [header, setHeader] = useState("");
+  const [body, setBody] = useState("");
+  const [totalQuestions, setTotalQuestions] = useState<number | null>(null);
+  const [questionRequirement, setQuestionRequirement] = useState<number | null>(null);
+  const [prize, setPrize] = useState<string | null>(null);
 
   useEffect(() => {
     sendGetTutorialRequest().then(res => {
@@ -40,45 +40,74 @@ const Quiz: React.FC<Props> = ({ quiz, history }) => {
     });
   }, [sendGetRaffleRequest]);
 
-  const generateRaffleContent = (questionRequirement: undefined, totalQuestions: undefined, prize: undefined) => {
+  const generateRaffleContent = (
+    questionRequirement: number | null,
+    totalQuestions: number | null,
+    prize: string | null
+  ) => {
     if (questionRequirement != null && prize != null) {
       return (
         <>
-        <IonCol size="12" className = "tutorial-info t-col">
-          {`Get ${questionRequirement} out of ${totalQuestions} questions and you could win a..`}
-        </IonCol>
-        <IonCol className="prize t-col" size="12" size-sm>
-          {prize}
-        </IonCol>
-        <IonCol size="12" className = "small" size-sm>
-          <p>Drawings are done monthly</p>
-        </IonCol>
+          <IonRow className="tutorial-row">
+            <IonCol size="9" className="tutorial-info t-col">
+              {`Get ${questionRequirement} out of ${totalQuestions} questions and you could win a..`}
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol className="prize t-col" size="12" size-sm>
+              {prize}
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol size="12" className="small" size-sm>
+              <p>Drawings are done monthly</p>
+            </IonCol>
+          </IonRow>
         </>
       );
     }
   };
 
-
   return (
     <IonPage>
       <IonContent fullscreen>
-      <IonImg className="tutorial-lines" src={TutorialLines}></IonImg>
+        <IonImg className="tutorial-lines" src={TutorialLines}></IonImg>
         <IonGrid className="tutorial-grid">
           <IonRow className="ion-align-items-start tutorial-row">
-            <IonCol className="tutorial-title" size="12" size-sm>
+            <IonCol className="tutorial-title" size-sm>
               <p>{header}</p>
             </IonCol>
-            <IonCol size="12" className="t-col">
-              <div className = "tutorial-info" dangerouslySetInnerHTML={{ __html: body }} />
-              <p className ="action-call">Find the answers within the powerhouse! </p>
-            </IonCol>
+          </IonRow>
 
-            {generateRaffleContent(questionRequirement, totalQuestions, prize)}
-            
-            <IonCol size="12">
-              <IonButton size="large" className="tutorial-button" onClick={() => {
-                 history.replace("/quiz/question");
-              }}>
+          <IonRow className="tutorial-row">
+            <IonCol size="10" className="t-col">
+              <div className="tutorial-info x" dangerouslySetInnerHTML={{ __html: body }} />
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol size="12" className="t-col">
+              <p className="action-call">Find the answers within the powerhouse! </p>
+            </IonCol>
+          </IonRow>
+
+          {generateRaffleContent(questionRequirement, totalQuestions, prize)}
+
+          <IonRow>
+            <IonCol size="12" className="termslink" size-sm>
+              <p>By pressing start you agree to our</p>
+              <a href="https://www.tinypowerhouse.org/"> terms and agreements</a>
+            </IonCol>
+            <IonCol>
+              <IonButton
+                size="large"
+                className="tutorial-button"
+                onClick={() => {
+                  history.replace("/quiz/question");
+                }}
+              >
                 Start Quiz!
               </IonButton>
             </IonCol>
